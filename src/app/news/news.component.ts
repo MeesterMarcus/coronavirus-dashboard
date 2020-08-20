f8dd41a83c5c514b78b5669d522d49adb573eb0a
MeesterMarcus <lorenzanamarcus@gmail.com>
2020-08-20 14:54:22 -0500

    filtering out articles that are not reputable or have nothing to do with covid

--- src/app/news/news.component.ts
+++ src/app/news/news.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {NewsService} from '../services/news.service';
-import {HttpParams} from '@angular/common/http';
 
 @Component({
   selector: 'app-news',
@@ -9,19 +8,42 @@ import {HttpParams} from '@angular/common/http';
 })
 export class NewsComponent implements OnInit {
 
-  articles: any;
+  articles = [];
   data: any;
 
-  constructor(private newsService: NewsService) { }
+  keywords = ['coronavirus', 'covid', 'masks', 'hospital', 'respirator',
+    'cases', 'deaths', 'death', 'vaccine', 'virus', 'outbreak', 'test'];
+
+  constructor(private newsService: NewsService) {
+  }
 
   ngOnInit(): void {
     const params = {};
     this.newsService.getNews(params).subscribe(
       data => {
         this.data = data;
-        this.articles = data.news;
+        const tempArray = [];
+        data.news.forEach(article => {
+          if (this.isGoodArticle(article)) {
+            tempArray.push(article);
+          }
+        });
+        this.articles = tempArray;
       }
     );
+  }
+
+  /**
+   * Filter article list, retrieving those that have images and match covid keywords
+   */
+  private isGoodArticle(article) {
+    let isMatching = false;
+    this.keywords.forEach(keyword => {
+      if (article.title.includes(keyword)) {
+        isMatching = true;
+      }
+    });
+    return isMatching && article.images && article.images.length > 0 && article.provider.name !== 'Fox News';
   }
 
 }
